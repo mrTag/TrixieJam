aa316cc77faceea3f9149df77c772aad19607206
Paul Lawitzki <p.lawitzki@gmail.com>
2017-05-06 14:55:16 +0200

    Paul erster Commit

---
+++ Assets/Scripts/CameraMan.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraMan : MonoBehaviour
+{
+	public static CameraMan instance;
+
+	public Transform[] CameraPositions;
+
+	private int mCurrentPosIndex = 0;
+
+	void Awake ()
+	{
+		instance = this;
+	}
+	
+	public void NextPos ()
+	{
+		mCurrentPosIndex++;
+		mCurrentPosIndex = Mathf.Clamp(mCurrentPosIndex, 0, CameraPositions.Length - 1);
+		transform.DOKill();
+		transform.DOMove(CameraPositions[mCurrentPosIndex].position, 1.5f).SetEase(Ease.InOutSine);
+	}
+
+	public void PreviousPos()
+	{
+		mCurrentPosIndex--;
+		mCurrentPosIndex = Mathf.Clamp(mCurrentPosIndex, 0, CameraPositions.Length - 1);
+		transform.DOKill();
+		transform.DOMove(CameraPositions[mCurrentPosIndex].position, 1.5f).SetEase(Ease.InOutSine);
+	}
+
+	#if UNITY_EDITOR
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.X))
+		{
+			NextPos();
+		}
+		else if (Input.GetKeyDown(KeyCode.C))
+		{
+			PreviousPos();
+		}
+	}
+	#endif
+}
