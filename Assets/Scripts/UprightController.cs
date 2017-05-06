fdf7d2c83a64b10622343aff8260bc49bdc7f910
dominik <dominik@chasing-carrots.com>
2017-05-06 17:09:28 +0200

    Jumping eingebaut, in der luft steuert es nicht mehr so stark...

--- Assets/Scripts/UprightController.cs
+++ Assets/Scripts/UprightController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class UprightController : MonoBehaviour {
 
 	public float UprightForce;
+	public float GroundCheckDist;
 	private Rigidbody rb;
 
 	void Start () {
@@ -13,9 +14,13 @@ public class UprightController : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
+		bool onGround = Physics.Raycast(transform.position, Vector3.down, GroundCheckDist);
+
 		float angleDiff = Vector3.Angle(Vector3.up, transform.up);
 		if(angleDiff != 0){
 			float springFactor = Mathf.InverseLerp(0, 90, angleDiff);
+			if(!onGround)
+				springFactor *= 0.1f;
 			Vector3 torqueAxis = Vector3.Cross(Vector3.up, transform.up);			
 			rb.AddTorque(-springFactor * torqueAxis * UprightForce * Time.fixedDeltaTime, ForceMode.Force);
 		}
