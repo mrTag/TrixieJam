f87ac2a4f6dd31bb020e44c93b17ba2f5eb37b29
Patrick Wachowiak <patrick@chasing-carrots.com>
2017-05-06 18:01:57 +0200

    trixi v1

---
+++ Assets/BoneControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneControl : MonoBehaviour {
+
+	public Transform ControlObject;
+	private Quaternion rotationOffset;
+	private Vector3 positionOffset;
+		
+	void Start () {
+		rotationOffset = Quaternion.FromToRotation(ControlObject.forward, transform.forward);
+		positionOffset = this.transform.position - ControlObject.transform.position;
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		this.transform.position = ControlObject.transform.position + positionOffset;
+		this.transform.rotation = ControlObject.transform.rotation * rotationOffset;
+	}
+}
