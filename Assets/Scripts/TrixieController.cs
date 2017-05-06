763b77df1a292689ebed9a731c86535b7951d9fc
dominik <dominik@chasing-carrots.com>
2017-05-06 14:29:42 +0200

    Character controller start

---
+++ Assets/Scripts/TrixieController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TrixiePart {
+	Front,
+	Back
+}
+
+[RequireComponent(typeof(Rigidbody))]
+public class TrixieController : MonoBehaviour {
+
+	public TrixiePart ControlsBodyPart;
+	public float MovementForce;
+	public Vector3 HorizMovementAxis = new Vector3(1,0,0);
+	public Vector3 VertMovementAxis = new Vector3(0,0,1);
+
+	private Rigidbody TrixieBody;
+	private string HorizontalInputAxis;
+	private string VerticalInputAxis;
+
+	// Use this for initialization
+	void Start () {
+		TrixieBody = GetComponent<Rigidbody>();
+		if(ControlsBodyPart == TrixiePart.Front){
+			HorizontalInputAxis = "HorizontalFront";
+			VerticalInputAxis = "VerticalFront";
+		}
+		else {
+			HorizontalInputAxis = "HorizontalBack";
+			VerticalInputAxis = "VerticalBack";
+		}
+		HorizMovementAxis.Normalize();
+		VertMovementAxis.Normalize();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 input = new Vector2(
+			Input.GetAxis(HorizontalInputAxis),
+			Input.GetAxis(VerticalInputAxis)
+		);
+		if(input.sqrMagnitude > 0){
+			Vector3 worldForce = (input.x * HorizMovementAxis) + (input.y * VertMovementAxis);
+			worldForce *= MovementForce;
+			TrixieBody.AddForce(worldForce * Time.fixedDeltaTime, ForceMode.Force);
+		}
+	}
+}
---
+++ Assets/Scripts/UprightController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class UprightController : MonoBehaviour {
+
+	public float UprightForce;
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+		float angleDiff = Vector3.Angle(Vector3.up, transform.up);
+		if(angleDiff != 0){
+			float springFactor = Mathf.InverseLerp(0, 90, angleDiff);
+			Vector3 torqueAxis = Vector3.Cross(Vector3.up, transform.up);			
+			rb.AddTorque(-springFactor * torqueAxis * UprightForce * Time.fixedDeltaTime, ForceMode.Force);
+		}
+	}
+}
