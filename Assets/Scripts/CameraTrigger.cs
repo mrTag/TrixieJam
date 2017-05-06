60f0c41e99ab7f1f8647fdc8ceb051ab9cbf36cb
Paul Lawitzki <p.lawitzki@gmail.com>
2017-05-06 17:50:20 +0200

    Anpassungen: Level und CameraTrigger

--- Assets/Scripts/CameraTrigger.cs
+++ Assets/Scripts/CameraTrigger.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class CameraTrigger : MonoBehaviour
 {
 	public int CameraPosIndex;
+	public Collider SolidifiedCollider;
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.transform.root.gameObject.CompareTag("trixie"))
+		{
 			CameraMan.instance.JumpToPos(CameraPosIndex);
+			if (SolidifiedCollider != null)
+			{
+				SolidifiedCollider.gameObject.SetActive(true);
+				SolidifiedCollider.isTrigger = false;
+			}
+			gameObject.SetActive(false);
+		}
 	}
 }
