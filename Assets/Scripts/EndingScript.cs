0614127f36d4aa831cac15553b5d3c3423e6b191
Paul Lawitzki <p.lawitzki@gmail.com>
2017-05-07 15:27:59 +0200

    Ending: erste iteration

---
+++ Assets/Scripts/EndingScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingScript : MonoBehaviour {
+
+	int _controllersInTrigger = 0;
+
+	void OnTriggerEnter(Collider other)
+	{
+		TrixieController ctrl = other.GetComponent<TrixieController>();
+		if (ctrl != null)
+		{
+			_controllersInTrigger++;
+			CheckEndingCondition();
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		TrixieController ctrl = other.GetComponent<TrixieController>();
+		if (ctrl != null)
+		{
+			_controllersInTrigger--;
+		}
+	}
+
+	void CheckEndingCondition()
+	{
+		if (_controllersInTrigger >= 2)
+		{
+			Debug.Log("END");
+		}
+	}
+}
